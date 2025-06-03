9f3188e082dfb4a9df0a445146512eef55c12a64
jkomyno <12381818+jkomyno@users.noreply.github.com>
2025-06-03 13:47:58 +0400

    chore: remove unused files

--- prisma.config.d.ts
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-import type { Env } from './src/env';
-declare const _default: import("prisma/config").PrismaConfigInternal<Env>;
-export default _default;
-//# sourceMappingURL=prisma.config.d.ts.map
